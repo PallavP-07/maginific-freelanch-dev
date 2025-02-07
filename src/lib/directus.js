65ad00903f01a232ec0f6ec5c5027f579ba3c507
PallavP-07 <pallav811@gmail.com>
2025-02-07 16:42:53 +0530

    build error fixed

--- src/lib/directus.js
+++ src/lib/directus.js
@@ -1,21 +1,17 @@
-import { createDirectus, rest } from '@directus/sdk';
+import { createDirectus, rest } from "@directus/sdk";
 import { readItems } from "@directus/sdk";
-const directus = createDirectus(`${process.env.NEXT_PUBLIC_DIRECTUS_API_URL}`).with(
-    rest({
-      onRequest: (options) => ({ ...options, cache: 'no-store' }),
-    })
-  );
-  //  export default directus;
-  export const fetchCollectionData = async (collection, queryParams = {}) => {
-    try {
-      const response = await directus.request(
-       readItems(collection,queryParams)
-      );
-      return {
-        response
-      }
-    } catch (error) {
-      console.error(`Error fetching ${collection}:`, error);
-      return { response: null };
-    }
-  };
+const directus = createDirectus(
+  `${process.env.NEXT_PUBLIC_DIRECTUS_API_URL}`
+).with(rest());
+//  export default directus;
+export const fetchCollectionData = async (collection, queryParams = {}) => {
+  try {
+    const response = await directus.request(readItems(collection, queryParams));
+    return {
+      response,
+    };
+  } catch (error) {
+    console.error(`Error fetching ${collection}:`, error);
+    return { response: null };
+  }
+};
